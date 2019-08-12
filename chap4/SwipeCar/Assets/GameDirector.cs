e21671cec245b3dd25fb9cb9039f242c8344b390
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-08-12 11:49:38 +0900

    add(car): attach script

--- chap4/SwipeCar/Assets/GameDirector.cs
+++ chap4/SwipeCar/Assets/GameDirector.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class GameDirector : MonoBehaviour {
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+        this.car = GameObject.Find ("car");
+        this.flag = GameObject.Find ("flag");
+        this.distance = GameObject.Find ("distance");
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        this.distance.GetComponent<Text> ().text = "ゴールまで" + length.ToString ("F2") + "m";
+
     }
 }
