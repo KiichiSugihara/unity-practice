b131cbbb3570c8ee607b95a29f56d4d52031a551
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-08-12 10:13:15 +0900

    feature(car): carSpeedController

--- chap4/SwipeCar/Assets/CarController.cs
+++ chap4/SwipeCar/Assets/CarController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
   float speed = 0;
+  Vector2 startPos;
   // Start is called before the first frame update
   void Start()
   {
@@ -16,7 +17,14 @@ public class CarController : MonoBehaviour
   {
     if (Input.GetMouseButtonDown(0))
     {
-      this.speed = 0.2f;
+      this.startPos = Input.mousePosition;
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+      Vector2 endPos = Input.mousePosition;
+      float swipeLength = endPos.x - this.startPos.x;
+
+      this.speed = swipeLength / 500.0f;
     }
 
     transform.Translate(this.speed, 0, 0);
