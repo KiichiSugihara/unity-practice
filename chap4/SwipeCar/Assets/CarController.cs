05a28458855dc1485ff87c952c916881e597fa56
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-08-12 11:54:04 +0900

    add(car): add carSound

--- chap4/SwipeCar/Assets/CarController.cs
+++ chap4/SwipeCar/Assets/CarController.cs
@@ -2,32 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
-{
+public class CarController : MonoBehaviour {
   float speed = 0;
   Vector2 startPos;
   // Start is called before the first frame update
-  void Start()
-  {
+  void Start () {
 
   }
 
   // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetMouseButtonDown(0))
-    {
+  void Update () {
+    if (Input.GetMouseButtonDown (0)) {
       this.startPos = Input.mousePosition;
-    }
-    else if (Input.GetMouseButtonUp(0))
-    {
+    } else if (Input.GetMouseButtonUp (0)) {
       Vector2 endPos = Input.mousePosition;
       float swipeLength = endPos.x - this.startPos.x;
 
       this.speed = swipeLength / 500.0f;
+
+      GetComponent<AudioSource> ().Play ();
     }
 
-    transform.Translate(this.speed, 0, 0);
+    transform.Translate (this.speed, 0, 0);
     this.speed *= 0.98f;
   }
 }
