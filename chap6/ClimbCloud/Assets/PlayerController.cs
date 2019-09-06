24a7ec4f1f1968768336fd72f929818cdc9888b4
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-09-06 12:51:06 +0900

    add(develop): add move

--- chap6/ClimbCloud/Assets/PlayerController.cs
+++ chap6/ClimbCloud/Assets/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
 
   Rigidbody2D rigid2D;
   float jumpForce = 680.0f;
-
+  float walkForce = 30.0f;
+  float maxWalkSpeed = 2.0f;
   // Start is called before the first frame update
   void Start()
   {
@@ -17,9 +18,23 @@ public class PlayerController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    //   jump
     if (Input.GetKeyDown(KeyCode.Space))
     {
       this.rigid2D.AddForce(transform.up * this.jumpForce);
+    }
+    // move right or left
+    int key = 0;
+    if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+    if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+    // speed
+    float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+    // limit speed
+    if (speedx < this.maxWalkSpeed)
+    {
+      this.rigid2D.AddForce(transform.right * key * this.walkForce);
     }
 
   }
